aec523ee2e02bf49765aabbcee2b953ae8e3cff1
alex.fan <alex.fan@okchem.com>
2019-09-26 16:44:11 +0800

    feature: toggle button component

---
+++ src/app/controls/toggle/toggle.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "toggle",
+  templateUrl: "./toggle.component.html",
+  styleUrls: ["./toggle.component.css"]
+})
+export class ToggleComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
